c029554e2e72823f2576f682e0cbc4f5d8d8af9d
ww5365 <ww5365@163.com>
2018-01-08 13:13:12 +0800

    modify gitignore

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main(int argc, char* arg[]){
 
-    cout<<"this is main entrance223346"<<endl;
+    cout<<"this is main entrance223346d"<<endl;
 
     two_dimension_array_test();
 
